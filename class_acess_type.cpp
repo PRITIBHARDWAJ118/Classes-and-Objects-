6efb2335a163ea17838495ca3e918d54da1a7f3f
Priti <pritibhardwaj118@gmail.com>
2024-08-31 22:36:35 +0530

    Add files via upload

---
+++ class_acess_type.cpp
@@ -0,0 +1,29 @@
+//Priti
+//23070123103
+//access types-public private 
+#include<iostream>
+using namespace std;
+class Rectangle 
+{
+    private://access type
+    double length;
+    double width;
+    public:
+    void setDimensions(double len,double wid) //setting the dimensions
+    {
+        length=len;
+        width=wid;
+    }
+    double calculateArea()//calculating area
+    {
+        return length *width;
+
+    }
+};
+int main()
+{
+    Rectangle rect;
+    rect.setDimensions(5.0,3.0);//calling the functions while giving the parameters 
+    cout<<"Area: "<<rect.calculateArea()<<endl;
+    return 0;
+}
---
+++ class_defining.cpp
@@ -0,0 +1,19 @@
+//Priti
+//23070123103
+//defining a class 
+#include<iostream>
+using namespace std;
+class Car //defining a class called car 
+{
+    public: //access type
+    string make="Kia";
+    string model="Seltos";
+    int year=2024;
+};
+int main()
+{
+    Car myCar; //creates an object of Car called myCar 
+    cout<<"Car details: "<<myCar.make<<" "<<myCar.model<<"("<<myCar.year<<")"<<endl;
+    return 0;
+}
+
